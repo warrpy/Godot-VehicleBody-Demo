31a1b28426a79bff0cdb5f002a5ef0ad16620825
Warrpy <80830641+warrpy@users.noreply.github.com>
2022-01-15 15:50:26 +0200

    Add files via upload

---
+++ Vehicle Tutorial/codes/vehicle_controller.gd
@@ -0,0 +1,60 @@
+extends VehicleBody
+
+# ------------------------------------------------
+# Script author: Bastiaan Olij, modified by Warrpy
+# ------------------------------------------------
+
+############################################################
+# behaviour values
+
+export var MAX_ENGINE_FORCE = 200.0
+export var MAX_BRAKE_FORCE = 5.0
+export var MAX_STEER_ANGLE = 0.5
+
+export var steer_speed = 5.0
+
+var steer_target = 0.0
+var steer_angle = 0.0
+
+############################################################
+# Input
+
+export var joy_steering = JOY_ANALOG_LX
+export var steering_mult = -1.0
+export var joy_throttle = JOY_ANALOG_R2
+export var throttle_mult = 1.0
+export var joy_brake = JOY_ANALOG_L2
+export var brake_mult = 1.0
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var steer_val = steering_mult * Input.get_joy_axis(0, joy_steering)
+	var throttle_val = throttle_mult * Input.get_joy_axis(0, joy_throttle)
+	var brake_val = brake_mult * Input.get_joy_axis(0, joy_brake)
+	
+	if Input.is_action_pressed("ui_up"):
+		throttle_val = 1.0
+	if Input.is_action_pressed("ui_down"):
+		throttle_val = -1.0
+	if Input.is_action_pressed("ui_accept"):
+		brake_val = 1.0
+	if Input.is_action_pressed("ui_left"):
+		steer_val = 1.0
+	if Input.is_action_pressed("ui_right"):
+		steer_val = -1.0
+	
+	engine_force = throttle_val * MAX_ENGINE_FORCE
+	brake = brake_val * MAX_BRAKE_FORCE
+	
+	steer_target = steer_val * MAX_STEER_ANGLE
+	if (steer_target < steer_angle):
+		steer_angle -= steer_speed * delta
+		if (steer_target > steer_angle):
+			steer_angle = steer_target
+	elif (steer_target > steer_angle):
+		steer_angle += steer_speed * delta
+		if (steer_target < steer_angle):
+			steer_angle = steer_target
+	steering = steer_angle
